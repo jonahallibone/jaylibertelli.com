c73075b5d563026ee931d4e4f874829ae9a1ae82
Jonah Allibone <jonah.allibone@gmail.com>
2021-12-24 19:11:37 -0500

    added dark mode

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,19 @@
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import {
   Box,
   Container,
   GridItem,
   HStack,
+  IconButton,
   SimpleGrid,
   Text,
+  useColorMode,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
 const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Box as="header" py={4} borderBottom="1px solid black">
       <Container maxW="7xl">
@@ -21,6 +26,13 @@ const Header = () => {
           <GridItem>
             <HStack as="nav" justifyContent="end">
               <Link href="/">Home</Link>
+              <IconButton
+                variant="outline"
+                aria-label="Call Sage"
+                fontSize="20px"
+                icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+                onClick={toggleColorMode}
+              />
             </HStack>
           </GridItem>
         </SimpleGrid>
